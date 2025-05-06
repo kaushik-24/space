74e082b9d46b156eaef7a0c93dacce7173677a20
kaushik-24 <grg23kaushik@gmail.com>
2025-05-06 12:14:08 +0545

    remove 3d models

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 
 import Word from "./components/Text";
+import Hero from "./section/Hero";
 import Space from "./section/Space";
 
 export default function Home() {
   return (
     <>
-    <Space />
+    <Hero />
     </>
      );
 }
